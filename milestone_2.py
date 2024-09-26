2c2722743b782b232242751c68d40f85ae791edf
El Shute <elshute2@gmail.com>
2024-09-26 14:12:46 +0100

    Adds milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,16 @@
+import random
+
+word_list = ["Apple", "Banana", "Orange", "Kiwi", "Melon"]
+
+print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = input("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess")
+else:
+    print("Oops! That is not a valid input.")
