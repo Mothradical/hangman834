a4205d266af6465f3ed73ff8f79663568f574c20
El Shute <elshute2@gmail.com>
2024-09-27 15:58:42 +0100

    milestone_4 given Docstring and minor code tweaks.py

--- milestone_4.py
+++ milestone_4.py
@@ -1,15 +1,29 @@
 import random
 
 class Hangman:
+    '''
+    This class is used to create conditions for a game a hangman.
+    
+    Attributes
+        word_list (list): a list of words
+        num_lives (int): the number of errors the player can make
+        word (str): a word randomly selected from word_list
+        word_guessed (list): a list of single-character strings that begins as underscores which are repalced by correctly guessed letters
+        num_letters: the number of unique letters remaining
+        list_of_guesses: an initially empty list that is appended with guessed letters
+    '''
     def __init__(self, word_list, num_lives = 5):
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
         self.word_guessed = ["_"] * len(self.word) # Contents to be replaced with correct guesses
-        self.num_letters = len(set(word_list))
+        self.num_letters = len(set(self.word))
         self.list_of_guesses = [] # New guesses are appended to this list
 
     def check_guess(self, guess):
+        '''
+        This function checks a guessed letter against a series of conditions.
+        '''
         guess = guess.lower()
         if guess in self.word:
             letter_indices = [index for index, letter in enumerate(self.word) if letter == guess]
@@ -22,8 +36,15 @@ class Hangman:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left.")
+            print(self.word_guessed)
     
     def ask_for_input(self):
+        '''
+        This function asks the player for inputs for the game of Hangman.
+        The function checks for the validity of the input before running the check_guess() function on the input.
+        It runs continuously until either break condition is satisfied, which depends on the outcomes of each instance of the check_guess() function.
+        This is the function to be accessed by the Hangman class object in order to commence a game of Hangman
+        '''
         while True:
             guess = input("Please guess a letter: ")
             if len(guess) != 1 or guess.isalpha() == False:
@@ -41,7 +62,7 @@ class Hangman:
                     print(f"Congratulations! You correctly identified the word as {self.word}")
                     break
 
-hangman_test = Hangman(["apple", "banana", "orange", "kiwi", "melon"], 5)
+# hangman_test = Hangman(["apple". "banana", "orange", "kiwi", "melon"], 5)
 
-hangman_test.ask_for_input()
+# hangman_test.ask_for_input()
 
